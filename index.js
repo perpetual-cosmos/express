e242f85ef055ff10a1b09e7cf003e8051e40e61c
perpetual-cosmos <prataptarunsingh241@gmail.com>
2024-10-02 22:53:46 +0530

    express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const port=3000;
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send('hello world')
+})
+app.get("/green",(req,res)=>{
+    res.send('hello world green')
+})
+app.get("/green",(req,res)=>{
+    res.send('hello world black')
+})
+app.get("/blue",(req,res)=>{
+    res.send('hello world blue')
+})
+app.listen(port,()=>{
+    console.log('i am a backend developer');
+});
