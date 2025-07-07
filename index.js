0663dc318dc5c2070132605d6a22801859748b88
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-07-07 12:26:22 +0530

    rainbow

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const express = require("express");
-const port=3000;
-const app=express();
+const port = 3000;
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send('hello world')
-})
-app.get("/green",(req,res)=>{
-    res.send('hello world green')
-})
-app.get("/green",(req,res)=>{
-    res.send('hello world black')
-})
-app.get("/blue",(req,res)=>{
-    res.send('hello world blue')
-})
-app.get("/red",(req,res)=>{
-    res.send('hello world red')
-})
-app.get("/violet",(req,res)=>{
-    res.send('hello world violet')
-})
-app.post("/",(req,res)=>{
-    a=req;
-    console.log(a)
-})
-app.listen(port,()=>{
-    console.log('i am a backend developer');
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.get("/green", (req, res) => {
+  res.send("hello world green");
+});
+app.get("/green", (req, res) => {
+  res.send("hello world black");
+});
+app.get("/blue", (req, res) => {
+  res.send("hello world blue");
+});
+app.get("/red", (req, res) => {
+  res.send("hello world red");
+});
+app.get("/violet", (req, res) => {
+  res.send("hello world violet");
+});
+
+app.get("/rainbow", (req, res) => {
+  res.send("hello world rainbow");
+});
+app.post("/", (req, res) => {
+  a = req;
+  console.log(a);
+});
+app.listen(port, () => {
+  console.log("i am a backend developer");
 });
